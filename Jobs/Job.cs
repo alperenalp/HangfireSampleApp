08d2f42ddc0d522e93ce941e3eb1904fec613f78
alperenalp <alperen.alp045@gmail.com>
2023-06-10 23:44:35 +0300

    Add project files.

---
+++ Jobs/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace HangfireUygulamasi.Jobs
+{
+    public class Job
+    {
+        public void SendNewProductsMailForUsers()
+        {
+            // Yeni ürünlerin listesi ve mail gönderme kodları burada olacak
+        }
+    }
+}
